8a351c5d93e70a542f2e6bd037640f519a292478
Terry-zh <496971418@qq.com>
2017-08-13 22:21:48 +0800

    Update TestPing.java

--- redis/TestPing.java
+++ redis/TestPing.java
@@ -5,6 +5,7 @@ import redis.clients.jedis.Jedis;
 public class TestPing {
 	public static void main(String[] args) 
 	{
+		// 测试是否成功连接redis、
 		Jedis jedis = new Jedis("127.0.0.1",6379);
 		System.out.println(jedis.ping());
 	}
