42c91f94cf3de3756638959475779ba22973476b
Terry-zh <496971418@qq.com>
2018-04-18 22:07:46 +0800

    [update unit test]

---
+++ nodeJS/nodeCross/unitTest.js
@@ -0,0 +1,12 @@
+module.exports = {
+  add: (...args) => {
+    return args.reduce((prev, curr) => {
+      return prev + curr
+    })
+  },
+  mul: (...args) => {
+    return args.reduce((prev, curr) => {
+      return prev * curr
+    })
+  }
+}
